75c4355578d1e6e71538540923ee7e4774934581
JarrettK <jkirck@hotmaill.com>
2022-04-06 20:19:23 -0400

    small changes

--- letterposition.js
+++ letterposition.js
@@ -1,1 +1,11 @@
-}}
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  return results;
+};
+
+letterPositions("lighthouse in the house")
