485f5be37e4b690b1e3991850016afd928849943
JarrettK <jkirck@hotmaill.com>
2022-04-11 12:02:20 -0400

    adding tests

--- head.js
+++ head.js
@@ -1,16 +1,9 @@
 //const assertEqual = require('./assertEqual');
 
-const assert = require('chai').assert;
-const head   = require('../head');
+const head = function (arr) {
+  console.log(arr[0]);
+};
 
-describe("#head", () => {
-  it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
-  });
-});
+head ([1,2,3,4,5,6,7,8,9,10,11])
 
-it("returns '5' for ['5']", () => {
-  assert.strictEqual(head(['5']), '5'); 
-});
-
-module.exports = assertEqual;
+module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,20 @@
 const head = require('../head');
+const assert = require('assert')
+const mocha = require('mocha')//??
 
 describe("#head", () => {
+  
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-});
 
-it("returns '5' for ['5']", () => {
-  assert.strictEqual(head(['5']), '5'); 
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns '3' for [3, 5, 7]", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
 });
 
 //assertEqual(head([5,6,7]), 5);
